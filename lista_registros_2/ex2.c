4578f79b4f281f37b936d31263ade12ad14fbe4f
Joao-Schio <jp@santoandrea.com.br>
2024-04-08 11:24:44 -0400

    deletando ex nao feito

--- lista_registros_2/ex2.c
+++
@@ -1,8 +0,0 @@
-/**
- * todos os codigos aqui podem ser compilados com o comando gcc <nome do arquivo> -o <nome do executavel> <flags>
- * flags que eu geralemente uso sao -Wall -Werror -pedantic
- * para executar usar o comando ./<nome do executavel>
-*/
-
-
-
